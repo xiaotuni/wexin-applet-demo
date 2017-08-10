17a616a032fd42877a5511f52323955346cd63a9
廖海兵 <liaohaibing@m24.cn>
2017-08-10 18:19:06 +0800

    a

--- components/Index.js
+++ components/Index.js
@@ -1,9 +1,6 @@
 import { Loading } from './loading/loading';
 import { Tabs } from './tabs/tabs';
 
-
-// export { Loading } from './loading/loading';
-// export { Tabs } from './tabs/tabs';
 const Coms = { Loading, Tabs }
 module.exports = Coms;
 
